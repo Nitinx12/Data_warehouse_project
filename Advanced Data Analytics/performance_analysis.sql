da1dce384bb1ef8af67e3bad88ed898326a4c4d4
Nitin <nitin321x@gmail.com>
2026-02-20 19:40:37 +0530

    Update performance_analysis.sql

--- Advanced Data Analytics/performance_analysis.sql
+++ Advanced Data Analytics/performance_analysis.sql
@@ -2,10 +2,10 @@ SELECT
 	order_year,
 	product_name,
 	current_sales,
-	AVG(current_sales) 
-		OVER(PARTITION BY product_name) AS avg_sales,
-	current_sales - AVG(current_sales) 
-						OVER(PARTITION BY product_name) AS diff_avg,
+	ROUND(AVG(current_sales) 
+		OVER(PARTITION BY product_name),2) AS avg_sales,
+	ROUND(current_sales - AVG(current_sales) 
+						OVER(PARTITION BY product_name),2) AS diff_avg,
 	CASE
 		WHEN current_sales - AVG(current_sales) 
 						OVER(PARTITION BY product_name) > 0
@@ -14,7 +14,18 @@ SELECT
 						OVER(PARTITION BY product_name) < 0
 						THEN 'Below Average'
 		ELSE 'Average'
-	END AS avg_change
+	END AS avg_change,
+	LAG(current_sales)
+		OVER(PARTITION BY product_name) AS py_sales,
+	CASE
+		WHEN current_sales - LAG(current_sales)
+								OVER(PARTITION BY product_name) > 0
+								THEN 'Increase'
+		WHEN current_sales - LAG(current_sales)
+								OVER(PARTITION BY product_name) < 0
+								THEN 'Decrease'
+		ELSE 'No change'
+	END AS py_changes								
 FROM(SELECT
 		EXTRACT(YEAR FROM S.order_date) AS order_year,
 		P.product_name,
